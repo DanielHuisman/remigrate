1c062d288a7e15b1938f9a6ea69c2f0909ce6f5f
Daniel Huisman <daniel@huisman.me>
2017-12-09 00:41:11 +0100

    Switch to command line commands

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
+import commandLineCommands from 'command-line-commands';
 import commandLineArgs from 'command-line-args';
 import commandLineUsage from 'command-line-usage';
 
 const pkg = require('../package.json');
 
+// Define command line commands
+const commandDefinitions = [null, 'help', 'version', 'create', 'up', 'down', 'down-all'];
+
 // Define command line options
 const optionDefinitions = [{
-    name: 'help',
-    alias: 'H',
-    type: Boolean,
-    description: 'Display usage information.'
-}, {
-    name: 'version',
-    alias: 'v',
-    type: Boolean,
-    description: 'Display version information.'
+    name: 'config',
+    alias: 'c',
+    type: String,
+    description: 'Path of the configuration file'
 }];
 
 // Define command line usage
@@ -21,26 +20,43 @@ const usageDefinition = [{
     header: 'remigrate',
     content: 'RethinkDB migrations.'
 }, {
+    header: 'Synopsis',
+    content: 'remigrate <options> <command>'
+}, {
+    header: 'Commands',
+    content: [
+        {name: 'create', summary: 'Create a new migration'},
+        {name: 'up', summary: 'Run all outstanding up migrations'},
+        {name: 'down', summary: 'Run one down migration'},
+        {name: 'down-all', summary: 'Run all down migrations'}
+    ]
+}, {
     header: 'Options',
     optionList: optionDefinitions
 }];
 
-// Parse options and usage definitions
-const options = commandLineArgs(optionDefinitions);
+// Parse command, options and usage definitions
+const {command, argv} = commandLineCommands(commandDefinitions);
+const options = commandLineArgs(optionDefinitions, argv);
 const usage = commandLineUsage(usageDefinition);
 
-if (options.help) {
-    // Display usage information
-    console.log(usage);
+switch (command) {
+    case null:
+    case 'help': {
+        // Display usage information
+        console.log(usage);
 
-    // Exit after printing information
-    process.exit(0);
-} else if (options.version) {
-    // Display version information
-    console.log(`remigrate - version ${pkg.version}`);
+        // Exit after printing information
+        process.exit(0);
+        break;
+    }
+    case 'version': {
+        // Display version information
+        console.log(`remigrate - version ${pkg.version}`);
 
-    // Exit after printing information
-    process.exit(0);
+        // Exit after printing information
+        process.exit(0);
+    }
 }
 
 // TODO: start the actual tool
