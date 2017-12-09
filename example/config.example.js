05383b595e537a74c1d112108987ecc1bdbfc82a
Daniel Huisman <daniel@huisman.me>
2017-12-09 14:36:49 +0100

    Improve example config

---
+++ example/config.example.js
@@ -0,0 +1,20 @@
+export default {
+    migrations: 'migrations',
+    template: 'es6-async',
+
+    driver: 'rethinkdb',
+    host: 'rethinkdb.example.com',
+    port: 28015,
+    db: 'example',
+    user: 'example',
+    password: 'example'
+
+    // driver: 'rethinkdbdash',
+    // db: 'example',
+    // user: 'example',
+    // password: 'example',
+    // servers: [{
+    //     host: 'rethinkdb.example.com',
+    //     port: 28015
+    // }]
+};
