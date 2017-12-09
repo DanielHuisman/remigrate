8ad7f4d2ef8967db33612548fab3c7c0798222da
Daniel Huisman <daniel@huisman.me>
2017-12-09 02:54:15 +0100

    Add basic example

---
+++ example/config.js
@@ -0,0 +1,12 @@
+export default {
+    migrations: 'migrations',
+
+    driver: 'rethinkdbdash',
+    db: 'example',
+    user: 'example',
+    password: 'example',
+    servers: [{
+        host: 'ssh.danielhuisman.io',
+        port: 28015
+    }]
+};
